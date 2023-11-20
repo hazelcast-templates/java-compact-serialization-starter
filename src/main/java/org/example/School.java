bdb74e2a361069ddeaa42cc53f55772aa7838486
Yuce Tekol <yuce.tekol@hazelcast.com>
2023-11-20 14:54:25 +0300

    Added POJOs

---
+++ src/main/java/org/example/School.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class School {
+    private int id;
+    private Classroom[] classrooms;
+
+    public School(int id, Classroom[] classrooms) {
+        this.id = id;
+        this.classrooms = classrooms;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Classroom[] getClassrooms() {
+        return classrooms;
+    }
+
+    public void setClassrooms(Classroom[] classrooms) {
+        this.classrooms = classrooms;
+    }
+}
